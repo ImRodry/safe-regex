cace6bbe760585213f4a44451b0599a0403de523
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-11-01 15:46:56 +0000

    feat: add TS type declarations

---
+++ typings/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Checks if a regex is safe to use in order to prevent catastrophic backtracking.
+ * @param regex can be a RegExp object or just a string.
+ * @param options Options for the check.
+ * `limit` - maximum number of allowed repetitions in the entire regex. Default: `25`.
+ */
+export default function safe(regex: string | RegExp, options?: Options): boolean;
+
+export type Options = Partial<{
+    /** Maximum number of allowed repetitions in the entire regex. Default `25` */
+    limit: number
+}> 
